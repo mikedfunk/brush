893733f9713714e33775e9ed68862e1e2b3c1d6d
Mike Funk <mike@mikefunk.com>
2022-12-27 22:34:15 -0800

    Use Zod for shape validation and types
    Validate shapes of objects at runtime _and_ define types without
    repeating ourselves.

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import Memcached from 'memcached'
 import * as dotenv from 'dotenv'
 import { unserializeSession } from 'php-unserialize'
 import cookieParser from 'cookie-parser'
+import { z } from 'zod'
 
 dotenv.config()
 
@@ -60,49 +61,49 @@ const memcachedGet = promisify(memcached.get).bind(memcached)
 // const memcachedDelete = promisify(memcached.del).bind(memcached)
 
 const SESSION_COOKIE_NAME = 'saatchisclocal'
-type Role = 'admin' | 'collector' | 'artist' | 'influencer' | 'guest'
-type ZendMeasurementUnit = 'in' | 'cm'
-type MeasurementSystem = 'INCH' | 'CENTIMETER'
-type FavoriteArtworkIds = Array<string>
 
-interface ZendSessionDataInterface {
-  adminFullName?: string
-  adminId?: number
-  checkInTimerStartedAt?: string
-  email: string
-  favorites: Array<number>
-  firstName: string
-  isAdmin: boolean
-  isAuthenticated: true
-  isKnownUser: boolean
-  isLimitedArtist: boolean
-  isSet?: boolean
-  isVerifiedToSell: boolean
-  lastName: string
-  localizationCountry: string
-  measurementUnit: ZendMeasurementUnit
-  userId: number
-  userName: string
-  userRole: Role
-  userTypeId: number
-}
+const measurementSystemParser = z.enum(['INCH', 'CENTIMETER']).optional()
+// const favoriteArtworkIdsParser = z.union([ // either or
+//   z.object({}),
+//   z.array(z.number())
+// ])
+const favoriteArtworkIdsParser = z.array(z.number()).catch([]) // if it's empty, it will come in as {}. Change that to [].
 
-interface SessionDataInterface extends ZendSessionDataInterface {
-  userFavorites: FavoriteArtworkIds,
-  measurementSystem: MeasurementSystem
-}
+const zendSessionDataParser = z.object({
+  admin_full_name: z.string().optional(),
+  adminId: z.number().optional(),
+  check_in_timer_started_at: z.string().optional(),
+  email: z.string(),
+  first_name: z.string(),
+  isEaselAdmin: z.boolean(),
+  is_limited_artist: z.coerce.boolean(), // comes in as 0|1
+  is_verified_to_sell: z.boolean().optional(),
+  last_name: z.string(),
+  id: z.number(),
+  username: z.string().nullable(),
+  user_type_id: z.number().gte(1).lte(2),
+})
 
-interface RawSessionDataInterface {
-  Zend_Auth?: {
-    storage: {
-      body: ZendSessionDataInterface
-    }
-  }
-  Saatchi?: {
-    measurementSystem: MeasurementSystem,
-    userFavorites: FavoriteArtworkIds,
-  }
-}
+const sessionDataParser = zendSessionDataParser.extend({
+  userFavorites: favoriteArtworkIdsParser,
+  measurementSystem: measurementSystemParser,
+})
+
+type SessionDataInterface = z.infer<typeof sessionDataParser>
+
+const rawSessionDataParser = z.object({
+  Zend_Auth: z.object({
+    storage: z.object({
+      body: zendSessionDataParser,
+    })
+  }).optional(),
+  Saatchi: z.object({
+    userFavorites: favoriteArtworkIdsParser,
+    measurementSystem: measurementSystemParser,
+  }).optional(),
+})
+
+type RawSessionDataInterface = z.infer<typeof rawSessionDataParser>
 
 class MemcachedUserDataNotFoundException extends Error {}
 
@@ -147,7 +148,8 @@ const getMemcachedUserData = async (
     throw new MemcachedUserDataNotFoundException('Memcached user data not found in session')
   }
 
-  const rawSessionData: RawSessionDataInterface = unserializeSession(serializedSessionData)
+  debugLog(JSON.stringify((unserializeSession(serializedSessionData))))
+  const rawSessionData: RawSessionDataInterface = rawSessionDataParser.parse(unserializeSession(serializedSessionData))
 
   if (!rawSessionData.Zend_Auth?.storage?.body) {
     throw new MemcachedUserDataUnserializationFailedException(
@@ -166,19 +168,78 @@ const getMemcachedUserData = async (
 }
 // }}}
 
-// types/interfaces {{{
-interface Artwork {
-
-}
-// }}}
-
 // palette api {{{
+
+const artworkDataParser = z.object({
+  artwork_id: z.string(),
+  is_legacy_artwork: z.boolean(),
+  legacy_user_art_id: z.number(),
+  user_id: z.number(),
+  created_at: z.number(),
+  uploaded_at: z.number(),
+  modified_at: z.number(),
+  year_produced: z.number(),
+  is_deleted: z.boolean(),
+  artwork_image: z.object({
+    main_url: z.string(),
+    thumbnail_url: z.string(),
+    polaroid_url: z.string(),
+    fullscreen_url: z.string(),
+    original_width: z.number(),
+    original_height: z.number(),
+    crops: z.object({
+      studio: z.object({
+        square: z.object({
+          x: z.number(),
+          y: z.number(),
+          width: z.number(),
+          height: z.number(),
+        }),
+        print: z.object({
+          x: z.number().nullable(),
+          y: z.number().nullable(),
+          width: z.number().nullable(),
+          height: z.number().nullable(),
+        }),
+      }),
+    }),
+  }),
+  additional_images: z.array(z.object({})),
+  youtube_video_id: z.string().nullable(),
+  title: z.string(),
+  width: z.string(),
+  height: z.string(),
+  depth: z.string(),
+  description: z.string(),
+  total_likes: z.number(),
+  total_views: z.number(),
+  total_curation_votes: z.number(),
+  category: z.string(),
+  subject: z.string(),
+  styles: z.array(z.string()),
+  mediums: z.array(z.string()),
+  keywords: z.array(z.string()),
+  materials: z.array(z.string()),
+  has_original: z.boolean(),
+  is_multipanel: z.boolean(),
+  panels: z.number(),
+  is_safe: z.boolean(),
+  visibility: z.enum(['published', 'draft']),
+  has_open_editions: z.boolean(),
+  has_limited_editions: z.boolean(),
+  cheapest_print_price: z.number().nullable(),
+  slug: z.string(),
+  url: z.string(),
+  products: z.array(z.object({})),
+})
+
+type ArtworkDataInterface = z.infer<typeof artworkDataParser>
 
 /**
  * @throws ArtworkNotFoundException
  * @throws HttpException
  */
-async function getArtworkDataByArtworkId (artworkId: string): Promise<Artwork> {
+async function getArtworkDataByArtworkId (artworkId: string): Promise<ArtworkDataInterface> {
   const response = await fetch(`${PALETTE_URL}/artwork/${artworkId}`)
 
   if (response.status === 404) {
@@ -190,6 +251,7 @@ async function getArtworkDataByArtworkId (artworkId: string): Promise<Artwork> {
   }
 
   const json = await response.json()
+  const artworkData = artworkDataParser.parse(json)
 
   return json
 }
