ae09959889532083fc02455b51fbd3dbf05d1558
Mike Funk <mike@mikefunk.com>
2023-01-15 17:29:41 -0800

    Extract session

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,8 @@ import * as http from 'http'
 import * as winston from 'winston'
 import * as expressWinston from 'express-winston'
 import cors from 'cors'
-import { promisify } from 'util'
-import Memcached from 'memcached'
 import * as dotenv from 'dotenv'
-import { unserializeSession } from 'php-unserialize'
 import cookieParser from 'cookie-parser'
-import { z } from 'zod'
-import { rawSessionDataParser, sessionDataParser } from './parsers/index.js'
 import {
   ArtworkNotFoundException,
   HttpException,
@@ -21,6 +16,7 @@ import {
   UserNotLoggedInException,
 } from './exceptions/index.js'
 import { getArtworkDataByArtworkId } from './paletteApi/index.js'
+import { getMemcachedUserData } from './session/index.js'
 
 dotenv.config()
 
@@ -42,69 +38,6 @@ const loggerOptions: expressWinston.LoggerOptions = {
 }
 
 app.use(expressWinston.logger(loggerOptions))
-// }}}
-
-// session {{{
-
-const memcached: Memcached = new Memcached('legacy.session.memcached:11211', {
-  // factor: 3, // Connection pool retry exponential backoff factor
-  failures: 3, // the number of failed-attempts to a server before it is regarded as 'dead'.
-  // maxTimeout: 5000, // 5 seconds, Connection pool retry max delay before retrying
-  // minTimeout: 1000, // 1 seconds, Connection pool retry min delay before retrying
-  // randomize: false, // Connection pool retry timeout randomization
-  reconnect: 10000, // 10 seconds, default is ( 18000000 ms => 18000 seconds => 300 minutes )
-  retries: 1, // Connection pool retries to pull connection from pool
-  timeout: 5000, // 5 seconds, time after which Memcached sends a connection timeout
-})
-const memcachedGet: (key: string) => Promise<any> = promisify(memcached.get).bind(memcached)
-// const memcachedSet = promisify(memcached.set).bind(memcached)
-// const memcachedDelete = promisify(memcached.del).bind(memcached)
-
-const SESSION_COOKIE_NAME = 'saatchisclocal'
-
-type SessionDataInterface = z.infer<typeof sessionDataParser>
-type RawSessionDataInterface = z.infer<typeof rawSessionDataParser>
-
-/**
- * @throws MemcachedUserDataNotFoundException
- * @throws MemcachedUserDataUnserializationFailedException
- * @throws UserNotLoggedInException
- */
-const getMemcachedUserData = async (
-  req: express.Request,
-  res: express.Response
-): Promise<express.Response<SessionDataInterface>> => {
-  const sessionCookieName: string | null = req.cookies[SESSION_COOKIE_NAME] ?? null
-
-  if (!sessionCookieName) {
-    throw new UserNotLoggedInException('User is not logged in')
-  }
-
-  const serializedSessionData = await memcachedGet(`memc.sess.saatchi_legacy.${sessionCookieName}`)
-
-  if (!serializedSessionData) {
-    throw new MemcachedUserDataNotFoundException('Memcached user data not found in session')
-  }
-
-  const rawSessionData: RawSessionDataInterface = rawSessionDataParser.parse(
-    unserializeSession(serializedSessionData)
-  )
-
-  if (!rawSessionData.Zend_Auth?.storage?.body) {
-    throw new MemcachedUserDataUnserializationFailedException(
-      `Failed to unserialize session data: ${serializedSessionData}`
-    )
-  }
-
-  const measurementSystem = rawSessionData?.Saatchi?.measurementSystem ?? 'INCH'
-  const zendUserFavorites = rawSessionData?.Saatchi?.userFavorites ?? {}
-
-  return res.json({
-    ...rawSessionData.Zend_Auth.storage.body,
-    measurementSystem,
-    userFavorites: Object.values(zendUserFavorites),
-  })
-}
 // }}}
 
 // routes {{{
---
+++ src/session/index.ts
@@ -0,0 +1,73 @@
+import express from 'express'
+import Memcached from 'memcached'
+import { unserializeSession } from 'php-unserialize'
+import { promisify } from 'util'
+import { z } from 'zod'
+import {
+  UserNotLoggedInException,
+  MemcachedUserDataNotFoundException,
+  MemcachedUserDataUnserializationFailedException,
+} from '../exceptions/index.js'
+import { rawSessionDataParser, sessionDataParser } from '../parsers/index.js'
+
+const memcached: Memcached = new Memcached('legacy.session.memcached:11211', {
+  // factor: 3, // Connection pool retry exponential backoff factor
+  failures: 3, // the number of failed-attempts to a server before it is regarded as 'dead'.
+  // maxTimeout: 5000, // 5 seconds, Connection pool retry max delay before retrying
+  // minTimeout: 1000, // 1 seconds, Connection pool retry min delay before retrying
+  // randomize: false, // Connection pool retry timeout randomization
+  reconnect: 10000, // 10 seconds, default is ( 18000000 ms => 18000 seconds => 300 minutes )
+  retries: 1, // Connection pool retries to pull connection from pool
+  timeout: 5000, // 5 seconds, time after which Memcached sends a connection timeout
+})
+const memcachedGet: (key: string) => Promise<any> = promisify(memcached.get).bind(memcached)
+// const memcachedSet = promisify(memcached.set).bind(memcached)
+// const memcachedDelete = promisify(memcached.del).bind(memcached)
+
+const SESSION_COOKIE_NAME = 'saatchisclocal'
+
+type SessionDataInterface = z.infer<typeof sessionDataParser>
+type RawSessionDataInterface = z.infer<typeof rawSessionDataParser>
+
+/**
+ * @throws MemcachedUserDataNotFoundException
+ * @throws MemcachedUserDataUnserializationFailedException
+ * @throws UserNotLoggedInException
+ */
+async function getMemcachedUserData(
+  req: express.Request,
+  res: express.Response
+): Promise<express.Response<SessionDataInterface>> {
+  const sessionCookieName: string | null = req.cookies[SESSION_COOKIE_NAME] ?? null
+
+  if (!sessionCookieName) {
+    throw new UserNotLoggedInException('User is not logged in')
+  }
+
+  const serializedSessionData = await memcachedGet(`memc.sess.saatchi_legacy.${sessionCookieName}`)
+
+  if (!serializedSessionData) {
+    throw new MemcachedUserDataNotFoundException('Memcached user data not found in session')
+  }
+
+  const rawSessionData: RawSessionDataInterface = rawSessionDataParser.parse(
+    unserializeSession(serializedSessionData)
+  )
+
+  if (!rawSessionData.Zend_Auth?.storage?.body) {
+    throw new MemcachedUserDataUnserializationFailedException(
+      `Failed to unserialize session data: ${serializedSessionData}`
+    )
+  }
+
+  const measurementSystem = rawSessionData?.Saatchi?.measurementSystem ?? 'INCH'
+  const zendUserFavorites = rawSessionData?.Saatchi?.userFavorites ?? {}
+
+  return res.json({
+    ...rawSessionData.Zend_Auth.storage.body,
+    measurementSystem,
+    userFavorites: Object.values(zendUserFavorites),
+  })
+}
+
+export { getMemcachedUserData }
