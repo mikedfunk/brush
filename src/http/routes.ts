e96ecef5fdbf1c17aa29a7a7dd846183215af0e2
Mike Funk <mike@mikefunk.com>
2023-03-21 15:14:50 -0700

    http proxy middleware, new tsconfig

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,10 +1,20 @@
-import { Application } from 'express'
 import { getArtwork } from './controller/artworkController.js'
 import { getAllClientData } from './controller/userController.js'
+import { Application, RequestHandler } from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
+function proxyTo(pathRewrite: (path: string) => string): RequestHandler
+{
+  return createProxyMiddleware({
+    target: 'http://paint.nginx',
+    changeOrigin: true,
+    pathRewrite: (path: string): string => pathRewrite(path),
+  })
+}
 
 function routes(app: Application): void {
   app.get('/all-client-data', getAllClientData)
-  app.get('/artwork/:artworkId', getArtwork)
+  app.get('/artwork/:artworkId', proxyTo((path: string) => path.replace('/artwork', '/browsing/artwork')))
 }
 
 export default routes
